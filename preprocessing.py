7f068eaf23d86af9ec97557a7c3cc4e7566c91b3
Arseni Muravjov <arsenimuravjov@MacBook-Air-Arseni.local>
2025-01-16 00:25:48 +0100

    preprocessing added

--- preprocessing.py
+++ preprocessing.py
@@ -13,10 +13,7 @@ latitudes = data.train['dd lat']
 longitudes = data.train['dd long']
 
 # Plot only one of the 10 subgroups (for now at least) to visualize
-print(set(species))
-for name in set(species):
-    print(name)
-new_species = [name for name in set(species) if name == b'bradypus_variegatus' or name == b'microryzomys_minutus']
+new_species = [name for name in set(species) if name == b'bradypus_variegatus_0' or name == b'microryzomys_minutus_0']
 print(new_species)
 plt.figure(figsize=(10, 6))
 for species_name in set(new_species):
@@ -42,3 +39,87 @@ plt.imshow(masked_layer, cmap='viridis', interpolation='nearest')
 plt.colorbar(label='Environmental Feature Value')
 plt.title('Environmental Feature Layer 0')
 plt.show()
+
+
+#####################
+### Preprocessing ###
+#####################
+
+"""
+First, change only use the two species examples that end with 0 (for now only maybe)
+"""
+
+species_train = data.train['species']
+species_test = data.test['species']
+
+
+exclusive_species = [b'microryzomys_minutus_0', b'bradypus_variegatus_0']
+
+train_filter = np.isin(species_train, exclusive_species)
+filtered_train = data.train[train_filter]
+
+test_filter = np.isin(species_test, exclusive_species)
+filtered_test = data.test[test_filter]
+
+print("\nFiltered Training Data Example:")
+print(filtered_train[:5])
+
+print("\nFiltered Test Data Example:")
+print(filtered_test[:5])
+
+
+"""
+Then deal with the missing data in coverages.
+"""
+
+coverages = data.coverages
+
+# Mask missing data for coverages
+masked_coverages = np.ma.masked_where(coverages == -9999, coverages)
+
+print("Before:")
+print(coverages[1, :, :])
+
+print("After:")
+print(masked_coverages[1, :, :])
+
+from sklearn.preprocessing import MinMaxScaler
+
+# scaler we use because data is not normally distributed!!
+scaler = MinMaxScaler()
+
+# template filled w -9999
+normalized_coverages = np.full(masked_coverages.shape, -9999, dtype=float)
+
+# Iterates through each layer
+for i in range(14):
+    layer = masked_coverages[i]
+
+    # Only normalize valid (non-masked) data
+    valid_mask = ~layer.mask
+    valid_values = layer[valid_mask]  # Extract valid values
+
+    # Scale valid values
+    scaled_values = scaler.fit_transform(valid_values.reshape(-1, 1)).flatten()
+
+    # Reintegrate into the layer with same dimensions
+    normalized_layer = np.full(layer.shape, -9999, dtype=float)
+    normalized_layer[valid_mask] = scaled_values
+
+    # Save back to normalized_coverages
+    normalized_coverages[i] = normalized_layer
+
+print("\nNormalized Coverages Example:")
+print(normalized_coverages[0, 500:505, 500:505])
+
+
+from sklearn.preprocessing import LabelEncoder
+
+# Encodes species as 0 or 1
+le = LabelEncoder()
+train_species = le.fit_transform(filtered_train['species'])
+test_species = le.transform(filtered_test['species'])
+
+print("\nSpecies Encoding Example:")
+print("Original Species Names:", filtered_train['species'][80:90])
+print("Encoded Species Labels:", train_species[80:90])
